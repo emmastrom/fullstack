5dcd4a92855daf9d3b8aa69d716ece8108b88b5b
Emma Lundström <1emma.lundstrom@gmail.com>
2024-04-30 00:07:21 +0300

    delete old code

--- osa2/src/App.jsx
+++ osa2/src/App.jsx
@@ -43,32 +43,6 @@ const Content = (props) => {
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
   const courses = [
     {
       name: 'Half Stack application development',
@@ -113,9 +87,6 @@ const App = () => {
       ]
     }
   ]
-  const exercises = course.parts.map(part => part.exercises)
-  //console.log(exercises)
-  const sum = exercises.reduce((prevValue, currentValue) => prevValue + currentValue, 0)
 
   return (
     <div>
