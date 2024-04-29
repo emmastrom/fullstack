cdff5050918dc6aaf016098245eb5cc8b718aa0d
Emma Lundström <1emma.lundstrom@gmail.com>
2024-04-30 00:05:38 +0300

    2.4

--- osa2/src/App.jsx
+++ osa2/src/App.jsx
@@ -4,12 +4,12 @@ const Header = (props) => {
 }
 
 const Total = (props) => {
-  console.log(props)
+  //console.log(props)
   return <b>total of {props.exercise} exercises</b>
 }
 
 const Part = (props) => {
-  console.log(props)
+  //console.log(props)
   return (
     <div>
       {props.part} {props.exercises}
@@ -18,10 +18,17 @@ const Part = (props) => {
 }
 
 const Course = (props) => {
-  //console.log(props)
+  console.log(props.parts.map(part => part.exercises))
+  let exercises = props.parts.map(part => part.exercises)
+  let sum = exercises.reduce((prevValue, currentValue) => prevValue + currentValue, 0)
   return (
     <div>
       <Header name={props.course} />
+      {props.parts.map(part =>
+      <Content key={part.id} part={part} />
+      )}
+      <br />
+      <Total exercise={sum} />
     </div>
   )
 }
@@ -62,17 +69,60 @@ const App = () => {
       }
     ]
   }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
   const exercises = course.parts.map(part => part.exercises)
-  console.log(exercises)
+  //console.log(exercises)
   const sum = exercises.reduce((prevValue, currentValue) => prevValue + currentValue, 0)
 
   return (
     <div>
-      <Course course={course.name} />
-      {course.parts.map(part =>
-        <Content key={part.id} part={part} />
+      <Header name={"Web development curriculum"} />
+        {courses.map(course =>
+        <Course key={course.id} course={course.name} parts={course.parts} />
         )}
-        <Total exercise={sum} />
     </div>
   )
 }
