16039da068442c2d2d34eac217f285d246886114
Emma Lundström <1emma.lundstrom@gmail.com>
2024-12-27 20:41:21 +0200

    2.8

--- osa2.6.-2.10/src/App.jsx
+++ osa2.6.-2.10/src/App.jsx
@@ -3,25 +3,34 @@ import Name from './components/Name'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1231244' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
     const nameObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
 
     const double = persons.filter((person) => person.name === newName).length
     const result = double === 0 ? setPersons(persons.concat(nameObject)) : alert(`${newName} is already added to phonebook`)
     setNewName('')
+    setNewNumber('')
     console.log(double)
   }
+
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -36,12 +45,19 @@ const App = () => {
             />
         </div>
         <div>
+          number:
+          <input
+            value={newNumber}
+            onChange={handleNumberChange}
+            />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
         {persons.map(person =>
-          <Name key={person.name} name={person} />
+          <Name key={person.name} name={person} number={person} />
         )}
     </div>
   )
--- osa2.6.-2.10/src/components/Name.jsx
+++ osa2.6.-2.10/src/components/Name.jsx
@@ -1,6 +1,6 @@
 const Name = ({ name }) => {
     return (
-      <ul>{name.name}</ul>
+      <ul>{name.name} {name.number}</ul>
     )
   }
   
