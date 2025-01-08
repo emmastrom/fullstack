ca931e7d5b53b0105ddab071e1f68b761af636c2
Emma Lundström <1emma.lundstrom@gmail.com>
2025-01-08 21:14:06 +0200

    2.9

--- osa2.6.-2.10/src/App.jsx
+++ osa2.6.-2.10/src/App.jsx
@@ -3,10 +3,15 @@ import Name from './components/Name'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1231244' }
+    { name: 'Arto Hellas', number: '040-1231244' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [searchName, setSearchName] = useState('')
+  const [showAll, setShowAll] = useState(false)
 
   const addName = (event) => {
     event.preventDefault()
@@ -33,10 +38,27 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleSearch = (event) => {
+    console.log(event.target.value)
+    setSearchName(event.target.value)
+  }
+
+  const personsToShow = showAll
+    ? persons
+    : persons.filter(person => person.name.toUpperCase().includes(searchName.toUpperCase()))
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with
+        <input
+        value={searchName}
+        onChange={handleSearch}
+        />
+      </div>
       <form onSubmit={addName}>
+        <h2>add a new</h2>
         <div>
           name: 
           <input
@@ -56,7 +78,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-        {persons.map(person =>
+        {personsToShow.map(person =>
           <Name key={person.name} name={person} number={person} />
         )}
     </div>
