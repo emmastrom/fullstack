8a81b5fe477ea255de4bfc4b253cad2ef6dd15e2
Emma Lundström <1emma.lundstrom@gmail.com>
2025-02-10 04:32:10 +0200

    4.8

---
+++ osa4_bloglist/tests/bloglist_api.test.js
@@ -0,0 +1,58 @@
+const { test, after, beforeEach } = require('node:test')
+const Blog = require('../models/blog')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+const initialBlogs = [
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0
+    }
+]
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    let blogObject = new Blog(initialBlogs[0])
+    await blogObject.save()
+    blogObject = new Blog(initialBlogs[1])
+    await blogObject.save()})
+
+test('bloglist is returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('there are two notes', async () => {
+    const response = await api.get('/api/blogs')
+  
+    assert.strictEqual(response.body.length, initialBlogs.length)
+  })
+  
+test('the first blog is by Michael Chan', async () => {
+    const response = await api.get('/api/blogs')
+  
+    const contents = response.body.map(e => e.author)
+    assert(contents.includes('Michael Chan'))
+  })
+
+after(async () => {
+  await mongoose.connection.close()
+})
