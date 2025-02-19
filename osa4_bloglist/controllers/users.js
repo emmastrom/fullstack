bd2308bc75cff5835fde4889504a21ea2abc2cd4
Emma Lundström <1emma.lundstrom@gmail.com>
2025-02-19 20:33:34 +0200

    4.15

--- osa4_bloglist/controllers/users.js
+++ osa4_bloglist/controllers/users.js
@@ -1,10 +1,10 @@
 const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
-const { nonExistingId } = require('../tests/test_helper')
 
 usersRouter.get('/', async (request, response) => {
-    const users = await User.find({})
+    const users = await User
+      .find({}).populate('blogs', {title: 1, author: 1, url: 1, likes: 1})
     response.json(users)
   })
 
@@ -20,15 +20,12 @@ usersRouter.post('/', async (request, response, next) => {
     passwordHash,
   })
 
-
   try {
     const savedUser = await user.save()
     response.status(201).json(savedUser)
   } catch (error) {
     next(error)
   }
-
-
 })
 
 module.exports = usersRouter
