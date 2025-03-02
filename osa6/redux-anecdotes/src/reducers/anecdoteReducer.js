9536947be6a98f8839a70470c31731340c186d07
Emma Lundström <1emma.lundstrom@gmail.com>
2025-03-03 00:44:02 +0200

    6.15

--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,23 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit"
 
-const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
-
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
   initialState: [],
   reducers: {
     createAnecdote(state, action) {
-      const anecdote = asObject(action.payload)
-      console.log(anecdote)
-      state.push(anecdote)
+      state.push(action.payload)
     },
     voteAnecdote(state, action) {
       const voted = {... action.payload, votes: action.payload.votes+1}
