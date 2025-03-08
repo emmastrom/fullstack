21b71a29952452720a87e124ddfed2f43957d3d3
Emma Lundström <1emma.lundstrom@gmail.com>
2025-03-08 15:35:56 +0200

    edit

--- osa4_bloglist/models/blog.js
+++ osa4_bloglist/models/blog.js
@@ -1,22 +1,31 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+
+const commentSchema = new mongoose.Schema({ content: String });
 
 const blogSchema = mongoose.Schema({
-  title: {type: String, required: true},
+  title: { type: String, required: true },
   author: String,
-  url: {type: String, required: true},
+  url: { type: String, required: true },
   likes: Number,
   user: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
-  }
-})
+    ref: "User",
+  },
+  comments: [commentSchema],
+});
 
-blogSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-      }
-})
+blogSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    if (returnedObject.comments) {
+      returnedObject.comments.map((comment) => {
+        comment.id = comment._id;
+        delete comment._id;
+      });
+    }
+  },
+});
 
-module.exports = mongoose.model('Blog', blogSchema)
+module.exports = mongoose.model("Blog", blogSchema);
