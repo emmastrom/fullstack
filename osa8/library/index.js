042a786ac3baaba4ae1134221981d704343a2929
Emma Lundström <1emma.lundstrom@gmail.com>
2025-03-10 21:39:57 +0200

    8.7

--- osa8/library/index.js
+++ osa8/library/index.js
@@ -128,6 +128,12 @@ const typeDefs = `
   author: String!
   genres: [String!]
   ): Book
+
+  editAuthor(
+  name: String!
+  setBornTo: Int!
+  ): Author
+
   }
 `
 
@@ -163,6 +169,15 @@ const resolvers = {
         authors = authors.concat(newAuthor)
       }
       return newBook
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find(a => a.name === args.name)
+      if (!author) {
+        return null
+      }
+      const updatedAuthor = { ...author, born: args.setBornTo }
+      authors = authors.map(a => (a.name === args.name ? updatedAuthor : a))
+      return updatedAuthor
     },
   },
 }
