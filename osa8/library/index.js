e12f3cbad05c7ee55675c88f0cef1b0fdc409d40
Emma Lundström <1emma.lundstrom@gmail.com>
2025-03-10 19:37:16 +0200

    8.6

--- osa8/library/index.js
+++ osa8/library/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require("@apollo/server")
 const { startStandaloneServer } = require("@apollo/server/standalone")
+const { v1: uuid } = require("uuid")
 
 let authors = [
   {
@@ -119,6 +120,15 @@ const typeDefs = `
   allBooks(author: String, genre: String): [Book!]
   allAuthors: [Author!]
   }
+
+  type Mutation {
+  addBook(
+  title: String!
+  published: Int!
+  author: String!
+  genres: [String!]
+  ): Book
+  }
 `
 
 const resolvers = {
@@ -143,6 +153,17 @@ const resolvers = {
   },
   Author: {
     bookCount: root => books.filter(book => book.author === root.name).length,
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      const newBook = { ...args, id: uuid() }
+      books = books.concat(newBook)
+      if (!authors.find(author => author.name === newBook.author)) {
+        const newAuthor = { name: newBook.author, id: uuid() }
+        authors = authors.concat(newAuthor)
+      }
+      return newBook
+    },
   },
 }
 
